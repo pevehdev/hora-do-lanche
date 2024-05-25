9ff5edd1d3b89f2499182b883ccb0f0f72f59004
pevehdev <pauloht2020@gmail.com>
2024-05-25 16:40:59 -0300

    Criado pagina de login com firebase

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from firebase_config import firebase_config
+import pyrebase
+
+app = Flask(__name__)
+app.secret_key = 'supersecretkey'
+
+firebase = pyrebase.initialize_app(firebase_config)
+auth = firebase.auth()
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            user = auth.sign_in_with_email_and_password(email, password)
+            return redirect(url_for('dashboard'))
+        except:
+            flash("Login failed. Check your credentials")
+            return redirect(url_for('login'))
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            user = auth.create_user_with_email_and_password(email, password)
+            flash("Account created successfully")
+            return redirect(url_for('login'))
+        except:
+            flash("Registration failed. Try again")
+            return redirect(url_for('register'))
+    return render_template('register.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return "Welcome to your dashboard!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ firebase_config.py
@@ -0,0 +1,12 @@
+
+firebase_config = {
+    'apiKey': "AIzaSyBCtpEqeFZ5LePTOrnavUOlzkRySc9slZQ",
+    'authDomain': "paozinho-auth.firebaseapp.com",
+    'databaseURL': "https://YOUR_PROJECT_ID.firebaseio.com",
+    'projectId': "paozinho-auth",
+    'storageBucket': "paozinho-auth.appspot.com",
+    'messagingSenderId':"830181816147",
+    'appId': "1:830181816147:web:44ec47f09d16e2febf53a6"
+}
+
+
